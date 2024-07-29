48e70ccc4a77b35feae62e4d93e9b24ff61128c3
cuongbv-11 <vc13050904@gmail.com>
2024-07-29 07:32:07 +0700

    authForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.scss";
+import AuthForm from "./pages/AuthForm/AuthForm";
+import Header from "./pages/Header";
+
+import { useEffect, useState } from "react";
+import { Product } from "./interfaces/Product";
+import { instance } from "./api/api";
 
 function App() {
-	return (
-		<>
-			<h1>Hello</h1>
-		</>
-	);
+  const [products, setProducts] = useState<Product[]>([]);
+  const nav = useNavigate();
+  const fetchProducts = async () => {
+    const { data } = await instance.get(`products`);
+    setProducts(data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  const handleRemove = async (id: any) => {
+    if (confirm("are you su")) {
+      await instance.delete(`products/${id}`);
+      setProducts(products.filter((item) => item.id !== id));
+    }
+  };
+  const onSubmitProduct = async (data: Product) => {
+    if (data.id) {
+      await instance.patch(`/products/${data.id}`, data);
+      fetchProducts();
+    } else {
+      const res = await instance.post(`/products`, data);
+      setProducts([...products, res.data]);
+    }
+    nav("/product");
+  };
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<AuthForm isLogin />} />
+        <Route path="/register" element={<AuthForm />} />
+        <Route path="/product" />
+        <Route path="/product-add" />
+        <Route path="/product-edit/:id" />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <ul className="text">
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
