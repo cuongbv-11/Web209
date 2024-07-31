bb0170a96a42a52873115d3b0621f3a4159b5341
cuongbv-11 <vc13050904@gmail.com>
2024-07-31 17:26:51 +0700

    cart

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Form from "./pages/Form/Form";
 import Home from "./pages/Home/Home";
 import Detail from "./pages/Home/Detail";
 import { StoreProvider } from "./Context/StoreContext";
+import Cart from "./pages/Cart/Cart";
 
 function App() {
   const nav = useNavigate();
@@ -19,6 +20,7 @@ function App() {
         <Route path="/login" element={<AuthForm isLogin />} />
         <Route path="/register" element={<AuthForm />} />
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/admin" element={<Dashboard />} />
         <Route path="/add" element={<Form />} />
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { StoreProvider } from "./Context/StoreContext";
 import { AuthProvider } from "./Context/AuthContext";
+import { CartProvider } from "./Context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <StoreProvider>
         <AuthProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </AuthProvider>
       </StoreProvider>
     </BrowserRouter>
